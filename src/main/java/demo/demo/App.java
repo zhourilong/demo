7c5775d3eb2c9f33e8811eb5c494ce96ebe9fc41
zhourilong <1043607081@qq.com>
2019-03-16 13:57:49 +0800

    added test

---
+++ src/main/java/demo/demo/App.java
@@ -0,0 +1,13 @@
+package demo.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
